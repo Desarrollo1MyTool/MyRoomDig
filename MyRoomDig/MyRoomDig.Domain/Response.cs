a5fe0bc2d0bd648c41a25bc7c10031e199e848fe
Laura Giraldo <desarrollo1.mytoolapp@outlook.com>
2019-06-12 13:13:43 -0500

    Clases e Imágenes

---
+++ MyRoomDig/MyRoomDig.Domain/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRoomDig.Domain
+{
+    class Response
+    {
+    }
+}
